3684f3ba89f0c461d2fa4831e5b07cf0a78832f4
ộ i i <vclmothaibabon1234@gmail.com>
2026-06-14 15:51:24 +0700

    updated one

--- movement.cs
+++ movement.cs
@@ -1,4 +1,7 @@
+using System;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 
 
@@ -9,45 +12,89 @@ public class movement : MonoBehaviour
     private SpriteRenderer sp;
     private Animator animator;
     private Rigidbody2D rb;
-    private bool isjumping = false;
+    private float jumpforce = 5f;
+    public Vector2 boxsize;
+    public float castdistance;
+    private float doublejumpforce = 5f;
+    private bool candoublejump ;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
         sp = GetComponent<SpriteRenderer>();
+       
     }
 
     // Update is called once per frame
     void Update()
     {
         horizontalinput = Input.GetAxisRaw("Horizontal");
-
         if (Input.GetKey(KeyCode.A))
         {
             sp.flipX = true;
-
+            animator.SetBool("isrunning", true);
         }
-        if (Input.GetKey(KeyCode.D))
+        else if (Input.GetKey(KeyCode.D))
         {
             sp.flipX = false;
+            animator.SetBool("isrunning", true);
         }
-
-        if (Input.GetKeyDown(KeyCode.Space)) 
+        else
         {
-            rb.linearVelocity = new Vector2(rb.linearVelocity.x, 5f);
-            isjumping = true;
+            animator.SetBool("isrunning", false);
         }
-    }
+         //neu an space va isgrounded = true thi thuc hien nhay
+        if (Input.GetButtonDown("Jump") && isgrounded())
+        {
+            jump(jumpforce);
+            animator.SetBool("isjumping", true);
+            animator.SetBool("isdoublejumping", false);
+        }
+        //neu nhu nut space da dc an, isgrounded = false va candoublejump = true
+        //thuc hien double jump
+        else if(Input.GetButtonDown("Jump") && !isgrounded() && candoublejump)
+            {
+            jump(doublejumpforce);
+            candoublejump = false;
+            animator.SetBool("isjumping", false);
+            animator.SetBool("isdoublejumping", true);
+        }
 
+    }
+    private void jump( float force)
+    {
+        rb.AddForce(Vector2.up * force, ForceMode2D.Impulse);
+
+    }
+    //neu nhu nv va cham ===> co the double jump
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isgrounded();
+        
+    }
+    public bool isgrounded()
+    {
+        //dung boxcast de check xem player co cham vao mat dat ko
+        bool hit = Physics2D.BoxCast(transform.position, boxsize, 0, -transform.up, castdistance, LayerMask.GetMask("ground"));
+       //neu nhu boxcast xac dinh nv da hoac dang cham vao mat dat ==> candoublejump = true
+        if (hit)
+        {
+            candoublejump = true;
+        }
+        return hit;
+    }
+    //check boxcast
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawCube(transform.position-transform.up * castdistance, boxsize);
+    }
     private void FixedUpdate()
     {
         rb.linearVelocity = new Vector2(horizontalinput * movespeed, rb.linearVelocity.y);
     }
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        isjumping = false;
-}
+  
+    
 }
 
 
